dce01ef4851f21798c10b9ce791e1fe8783c1675
Lee <leejavaa@gmail.com>
2022-10-16 12:30:32 +0200

    API calls updated

--- RemoteControl/frontend/src/components/Control.jsx
+++ RemoteControl/frontend/src/components/Control.jsx
@@ -4,25 +4,51 @@ import axiosInstance from "../axios";
 import car from "../static/images/Car.png";
 
 export const Control = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({
+    leftDistance: 100,
+    rightDistance: 100,
+    leftCurrent: 150,
+    rightCurrent: 150,
+    batteryVoltage: 7.2,
+  });
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      axiosInstance
-        .get(``)
-        .then((res) => {
-          setData(res.data);
-          console.log(data);
-        })
-        .catch((err) => console.log(err.message));
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  //   useEffect(() => {
+  //     const interval = setInterval(() => {
+  //       axiosInstance
+  //         .get(``)
+  //         .then((res) => {
+  //           setData(res.data);
+  //           console.log(data);
+  //         })
+  //         .catch((err) => console.log(err.message));
+  //     }, 1000);
+  //     return () => {
+  //       clearInterval(interval);
+  //     };
+  //   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const setSpeed = (speed, wheel) => {
+    if (wheel === "right") {
+      //   axiosInstance
+      //     .get(`control/right/${speed}`)
+      //     .then((res) => {
+      //       console.log(res);
+      //     })
+      //     .catch((err) => console.log(err.message));
+      document.getElementById("rightRange").value = speed;
+    } else {
+      //   axiosInstance
+      //     .get(`control/left/${speed}`)
+      //     .then((res) => {
+      //       console.log(res);
+      //     })
+      //     .catch((err) => console.log(err.message));
+      document.getElementById("leftRange").value = speed;
+    }
+  };
 
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen bg-offWhite">
       <div className="w-full right-0 text-right px-8 font-medium text-xl">
         <Link to="help/">
           <p className="hover:underline hover:text-purpLight decoration-purpLight">
@@ -37,13 +63,32 @@ export const Control = () => {
       <div className="flex w-10/12 h-2/3 mx-auto mt-14 justify-around">
         <div className="flex justify-center items-center w-1/4 ">
           <div className="grid grid-cols-1 gap-y-7">
-            <div className="bg-dark text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
-              Object Distance: 82cm
-            </div>
-            <button>
-              <p className="text-4xl font-semibold text-white font-4xl bg-purpLight inline-block w-full py-4 rounded-2xl">
-                Full
-              </p>
+            {data.leftDistance <= 20 && (
+              <div className="bg-sensorDanger text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Object Distance: {data.leftDistance}cm
+              </div>
+            )}
+            {data.leftDistance > 20 && data.leftDistance <= 70 && (
+              <div className="bg-sensorWarning text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Object Distance: {data.leftDistance}cm
+              </div>
+            )}
+            {data.leftDistance > 70 && data.leftDistance <= 99 && (
+              <div className="bg-sensorOk text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Object Distance: {data.leftDistance}cm
+              </div>
+            )}
+            {data.leftDistance === 100 && (
+              <div className="bg-sensorOk text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Object Distance: Max
+              </div>
+            )}
+            <button
+              type="submit"
+              className="bg-dark py-4 rounded-2xl hover:bg-purpLight"
+              onClick={() => setSpeed(15, "left")}
+            >
+              <p className="text-4xl font-semibold text-white font-4xl">Full</p>
             </button>
             <input
               type="range"
@@ -53,32 +98,79 @@ export const Control = () => {
               step="1"
               className="h-44 w-2 mx-auto"
               style={{ webkitAppearance: "slider-vertical" }}
+              id="leftRange"
+              onChange={(e) => setSpeed(e.target.value, "left")}
             />
-            <button>
-              <p className="text-4xl font-semibold text-white font-4xl bg-purpLight inline-block w-full py-4 rounded-2xl">
-                Stop
-              </p>
+            <button
+              type="submit"
+              className="bg-dark py-4 rounded-2xl hover:bg-purpLight"
+              onClick={() => setSpeed(0, "left")}
+            >
+              <p className="text-4xl font-semibold text-white font-4xl">Stop</p>
             </button>
-            <div className="bg-dark text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
-              Current Draw: 150mA
-            </div>
+            {data.leftCurrent <= 200 && (
+              <div className="bg-sensorOk text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Current Draw: {data.leftCurrent}mA
+              </div>
+            )}
+            {data.leftCurrent > 200 && data.leftCurrent <= 500 && (
+              <div className="bg-sensorWarning text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Current Draw: {data.leftCurrent}mA
+              </div>
+            )}
+            {data.leftCurrent > 500 && (
+              <div className="bg-sensorDanger text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Current Draw: {data.leftCurrent}mA
+              </div>
+            )}
           </div>
         </div>
         <div className="w-1/2 grid place-items-center">
           <img src={car} alt="Car" />
-          <div className="bg-dark text-white text-center px-2 py-1 rounded-xl text-xl font-semibold -mt-40">
-            Battery Voltage: 7.00V
-          </div>
+          {data.batteryVoltage > 6.8 && (
+            <div className="bg-sensorOk text-white text-center px-2 py-1 rounded-xl text-xl font-semibold -mt-40">
+              Battery Voltage: {data.batteryVoltage}V
+            </div>
+          )}
+          {data.batteryVoltage <= 6.8 && data.batteryVoltage > 6.2 && (
+            <div className="bg-sensorWarning text-white text-center px-2 py-1 rounded-xl text-xl font-semibold -mt-40">
+              Battery Voltage: {data.batteryVoltage}V
+            </div>
+          )}
+          {data.batteryVoltage <= 6.2 && (
+            <div className="bg-sensorDanger text-white text-center px-2 py-1 rounded-xl text-xl font-semibold -mt-40">
+              Battery Voltage: {data.batteryVoltage}V
+            </div>
+          )}
         </div>
         <div className="flex justify-center items-center w-1/4">
           <div className="grid grid-cols-1 gap-y-7">
-            <div className="bg-dark text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
-              Object Distance: 82cm
-            </div>
-            <button>
-              <p className="text-4xl font-semibold text-white font-4xl bg-purpLight inline-block w-full py-4 rounded-2xl">
-                Full
-              </p>
+            {data.rightDistance <= 20 && (
+              <div className="bg-sensorDanger text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Object Distance: {data.rightDistance}cm
+              </div>
+            )}
+            {data.rightDistance > 20 && data.rightDistance <= 70 && (
+              <div className="bg-sensorWarning text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Object Distance: {data.rightDistance}cm
+              </div>
+            )}
+            {data.rightDistance > 70 && data.rightDistance <= 99 && (
+              <div className="bg-sensorOk text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Object Distance: {data.rightDistance}cm
+              </div>
+            )}
+            {data.rightDistance === 100 && (
+              <div className="bg-sensorOk text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Object Distance: Max
+              </div>
+            )}
+            <button
+              type="submit"
+              className="bg-dark py-4 rounded-2xl hover:bg-purpLight"
+              onClick={() => setSpeed(15, "right")}
+            >
+              <p className="text-4xl font-semibold text-white font-4xl">Full</p>
             </button>
             <input
               type="range"
@@ -88,15 +180,31 @@ export const Control = () => {
               step="1"
               className="h-44 w-2 mx-auto"
               style={{ webkitAppearance: "slider-vertical" }}
+              id="rightRange"
+              onChange={(e) => setSpeed(e.target.value, "right")}
             />
-            <button>
-              <p className="text-4xl font-semibold text-white font-4xl bg-purpLight inline-block w-full py-4 rounded-2xl">
-                Stop
-              </p>
+            <button
+              type="submit"
+              className="bg-dark py-4 rounded-2xl hover:bg-purpLight"
+              onClick={() => setSpeed(0, "right")}
+            >
+              <p className="text-4xl font-semibold text-white font-4xl">Stop</p>
             </button>
-            <div className="bg-dark text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
-              Current Draw: 150mA
-            </div>
+            {data.rightCurrent <= 200 && (
+              <div className="bg-sensorOk text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Current Draw: {data.rightCurrent}mA
+              </div>
+            )}
+            {data.rightCurrent > 200 && data.rightCurrent <= 500 && (
+              <div className="bg-sensorWarning text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Current Draw: {data.rightCurrent}mA
+              </div>
+            )}
+            {data.rightCurrent > 500 && (
+              <div className="bg-sensorDanger text-white text-center px-2 py-1 rounded-xl text-xl font-semibold">
+                Current Draw: {data.rightCurrent}mA
+              </div>
+            )}
           </div>
         </div>
       </div>
--- RemoteControl/frontend/src/components/Help.jsx
+++ RemoteControl/frontend/src/components/Help.jsx
@@ -3,30 +3,46 @@ import { Link } from "react-router-dom";
 
 export const Help = () => {
   return (
-    <div>
-      <Link to="/">Back</Link>
-      <div>
-        Control the left and right wheels with the control bars on the left and
-        right side respectively. The “Full” button pushes the car to max speed
-        and the “Stop” button brings the wheel speed down to 0. Move the slide
-        bar up to incrementally increase the speed and bring it down to
-        incrementally decrease the speed.
+    <div className="bg-offWhite">
+      <div className="w-full px-8 font-medium text-xl absolute">
+        <Link to="/">
+          <p className="hover:underline hover:text-purpLight decoration-purpLight">
+            Back
+          </p>
+        </Link>
       </div>
-      <div>
-        <li>
-          <ul>
-            Object Detection of each wheel is displayed above the control bars
-            on each side of the car. The object detection sign changes colour as
-            objects come closer to the car as a warning.
-          </ul>
-          <ul>
-            The current draw of each wheel is shown below the control bars on
-            each side of the car.
-          </ul>
-          <ul>
-            The battery voltage is displayed right below the battery on the car.{" "}
-          </ul>
-        </li>
+      <div className="w-full h-screen grid place-items-center">
+        <div className="grid grid-cols-1 gap-y-2 w-3/5">
+          <h2 className="font-semibold text-xl -mb-2">Car Control</h2>
+          <div className="bg-purpLight text-white text-justify px-4 py-4 rounded-xl">
+            Control the left and right wheels with the control bars on the left
+            and right side respectively. The “Full” button pushes the car to max
+            speed and the “Stop” button brings the wheel speed down to 0. Move
+            the slide bar up to incrementally increase the speed and bring it
+            down to incrementally decrease the speed.
+          </div>
+          <h2 className="font-semibold text-xl -mb-2">Sensor Data</h2>
+          <div className="bg-purpLight text-white text-justify px-4 py-4 rounded-xl">
+            <ul className="ml-4 list-disc text-left">
+              <li>
+                Object Detection of each wheel is displayed above the control
+                bars on each side of the car.
+              </li>
+              <li>
+                The current draw of each wheel is shown below the control bars
+                on each side of the car.
+              </li>
+              <li>
+                The battery voltage is displayed right below the battery on the
+                car.
+              </li>
+              <li>
+                You will see that these sensors change colour intuitively at
+                different values.
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
